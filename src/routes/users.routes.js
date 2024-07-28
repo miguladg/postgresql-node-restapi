1094c1e8fcfcb70371ecb5f7731912a84eb15e04
migueladg <migul.a.d.g@gmail.com>
2024-07-28 18:21:06 -0500

    CRUD OK

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,52 +1,58 @@
 import { Router } from "express";
-import {pool} from "../db.js";
+import { pool } from "../db.js";
 
 const router = Router();
 
-router.get('/users', async (req, res) => {
-    const {rows} = await pool.query('SELECT * FROM users')
-    // console.log(result);
-    res.json(rows);
+router.get("/users", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM users");
+  res.json(rows);
+});
 
-})
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
 
-router.get('/users/:id', async (req, res) => {
-    const {id} = req.params
-    const {rows} = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+  if (rows.length === 0) {
+    return res.status(404).json({
+      mesange: "don found the user",
+    });
+  }
 
-    if (rows.length === 0) {
-        return res.status(404).json(
-            {
-                mesange: 'don found the user'
-            }
-        );
-    }
+  // res.send('obteniendo usuarios' + id)
+  res.json(rows[0]);
+});
 
-    // res.send('obteniendo usuarios' + id)
-    res.json(rows);
-})
-
-router.post('/users', async (req, res) => {
-
-    const {rows} = await pool.query('INSERT INTO useres (name, email, created_at) VALUES (1, )');
-    res.send('creando usuario')
-})
+router.post("/users", async (req, res) => {
+  const data = req.body;
+  const { rows } = await pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+    [data.name, data.email]
+  );
+  res.json(rows[0]);
+});
 
 router.delete('/users/:id', async (req, res) => {
     const {id} = req.params
-    const {rows} = await pool.query('DELETE FROM users WHERE id = $1', [d1]);
+    const {rows, rowCount} = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
 
-    if (rows.length === 0) {
-        return res.status(429).json({
-            message: "Don't found the users for delete"
+    if (rowCount === 0) {
+        return res.status(404).json({
+            message: "User not found"
         });
-    }
-    
-    res.json(rows);
+    } 
+    res.status(204);
 })
 
-router.put('/users/:id', (req, res) => {
-    res.send('Actualizando usuario')
-})
+router.put("/users/:id", async (req, res) => {
+    const { id } = req.params
+    const data = req.body
+    const {rows, rowCount} = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [data.name, data.email, id])
+    if (rowCount === 0) {
+        return res.status(429).json({
+            message: "User not found"
+        })
+    }
+    res.json(rows[0]);
+});
 
 export default router;
