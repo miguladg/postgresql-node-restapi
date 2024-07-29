bb8bd536407d609da96e2d325f9bd800aa908a0a
migueladg <migul.a.d.g@gmail.com>
2024-07-28 19:24:07 -0500

    all is OK, by miguel

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
-export const PORT = 4000;
+export const DB_USER = process.env.DB_HOST
+export const DB_HOST = process.env.DB_HOST
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_PORT = process.env.DB_PORT
+
+export const PORT = process.env.PORT || 4000;
